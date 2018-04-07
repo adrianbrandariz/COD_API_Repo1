073f2ffb53277d62538b7449c5cce56ff731d1de
Hansen@DESKTOP-P1ST9AA <hansennygard@gmail.com>
2018-04-07 16:51:24 +0200

    Imports añadidos y método para crear repositorio implementado

--- src/main/java/com/mycompany/pruebamaven/Metodos.java
+++ src/main/java/com/mycompany/pruebamaven/Metodos.java
@@ -1,9 +1,31 @@
 package com.mycompany.pruebamaven;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
 /**
  *
  * @author abrandarizdominguez
  */
 public class Metodos {
 
+    public void crearRepositorio() {
+        try {
+            String login = JOptionPane.showInputDialog("GitHub User:");
+            String password = JOptionPane.showInputDialog("GitHub Password:");
+            GitHub github = GitHub.connectUsingPassword(login, password);
+            String repositoryName = JOptionPane.showInputDialog("Repository Name:");
+            String description = JOptionPane.showInputDialog("GitHub description:");
+            GHRepository newRepository = github.createRepository(
+                    repositoryName, 
+                    description, 
+                    "http://www.github.com/adrianbrandariz/api", true);
+        } catch (IOException ex) {
+            Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
