cac0f385410b29c0d5967895c6c290fee2bfe4e7
Hansen@DESKTOP-P1ST9AA <hansennygard@gmail.com>
2018-04-07 16:50:34 +0200

    Main con método "crearRepositorio" sñadido

--- src/main/java/com/mycompany/pruebamaven/Main.java
+++ src/main/java/com/mycompany/pruebamaven/Main.java
@@ -1,13 +1,16 @@
 package com.mycompany.pruebamaven;
 
+import java.io.IOException;
+
 /**
  *
  * @author abrandarizdominguez
  */
 public class Main {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+         Metodos obj = new Metodos();
+         obj.crearRepositorio();
     }
     
 }
